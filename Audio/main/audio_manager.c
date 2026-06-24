66d47f3d6419cb368680c0371c454ad888479e43
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2026-06-24 08:53:05 -0300

    feat[2.2]: implementa mock inicial do atuador de áudio

---
+++ Audio/main/audio_manager.c
@@ -0,0 +1,41 @@
+#include "audio_manager.h"
+
+#include "esp_log.h"
+
+static const char *TAG = "AUDIO";
+
+static uint8_t current_volume = 100;
+
+void audio_init(void) {
+    ESP_LOGI(TAG, "Inicializando atuador de áudio...");
+    ESP_LOGI(TAG, "Volume inicial: %d%%", current_volume);
+    ESP_LOGI(TAG, "Atuador de áudio inicializado!");
+}
+
+
+void audio_play(void) {
+    ESP_LOGI(TAG, "Reproduzindo áudio!");
+}
+
+
+void audio_stop(void) {
+    ESP_LOGI(TAG, "Parando áudio!");
+}
+
+
+void audio_set_volume(uint8_t volume) {
+    if(volume < 0) {
+        ESP_LOGW(TAG, "Volume %d inválido! Ajustando para 0.", volume);
+        current_volume = 0;
+    }
+
+    else if(volume > 100) {
+        ESP_LOGW(TAG, "Volume %d inválido! Ajustando para 100.", volume);
+        current_volume = 100;
+    }
+
+    else {
+        ESP_LOGI(TAG, "Alterando o volume atual de %d para %d%%", current_volume, volume);
+        current_volume = volume;
+    }
+}
