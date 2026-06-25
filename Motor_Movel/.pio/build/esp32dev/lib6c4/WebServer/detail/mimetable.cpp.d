05c97727417ee44f48a3eff73f13ee9bb6ca883f
Leonardo Patias Johansson <leonardojohansson@hotmail.com>
2026-06-25 11:07:58 -0300

    fix[2.2]: Corrigindo o motor móvel para rodar no ambiente ESP

---
+++ Motor_Movel/.pio/build/esp32dev/lib6c4/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib6c4/WebServer/detail/mimetable.cpp.o: \
+ /home/leonardopj/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ /home/leonardopj/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ /home/leonardopj/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
