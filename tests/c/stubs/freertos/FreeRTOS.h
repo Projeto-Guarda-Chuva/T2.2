07dbe2a8febbfdbd884c39db868550f98bb48945
anamachadoc <anaclaudiamachado211@gmail.com>
2026-07-02 21:29:16 +0000

    delete old tests

--- tests/c/stubs/freertos/FreeRTOS.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-typedef int BaseType_t;
-typedef unsigned int TickType_t;
-
-#define pdPASS 1
-#define pdMS_TO_TICKS(ms) (ms)
--- tests/c/stubs/nvs_flash.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "esp_err.h"
-
-#define ESP_ERR_NVS_NO_FREE_PAGES 0x1001
-#define ESP_ERR_NVS_NEW_VERSION_FOUND 0x1002
-
-esp_err_t nvs_flash_init(void);
-esp_err_t nvs_flash_erase(void);
