247f9f6a1b0cba26877d9a3b6431be2bf90b5cd1
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2026-07-02 07:33:02 -0300

    fix[2.2]: altera interface do atuador de áudio

--- Audio/main/audio_manager.c
+++ Audio/main/audio_manager.c
@@ -1,10 +1,16 @@
 #include "audio_manager.h"
 
+#include <string.h>
 #include "esp_log.h"
 
 static const char *TAG = "AUDIO";
 
 static uint8_t current_volume = 100;
+
+static bool playing = false;
+
+static char current_file[64] = "";
+
 
 void audio_init(void) {
     ESP_LOGI(TAG, "Inicializando atuador de áudio...");
@@ -13,12 +19,40 @@ void audio_init(void) {
 }
 
 
-void audio_play(void) {
-    ESP_LOGI(TAG, "Reproduzindo áudio!");
+bool audio_is_playing(void) {
+    return playing;
+}
+
+
+void audio_play_default(void) {
+    ESP_LOGI(TAG, "Reproduzindo áudio padrão!");
+    audio_play("default.mp3");
+}
+
+void audio_play(const char *file) {
+    if(file == NULL || strlen(file) == 0) {
+        ESP_LOGE(TAG, "Nome do arquivo inválido!");
+        return;
+    }
+
+    strncpy(current_file, file, sizeof(current_file)-1);
+    current_file[sizeof(current_file)-1] = '\0';
+
+    playing = true;
+    ESP_LOGI(TAG, 
+        "Reproduzindo arquivo \"%s\" (Volume: %d%%)", 
+        current_file, 
+        current_volume);
 }
 
 
 void audio_stop(void) {
+    if(!playing) {
+        ESP_LOGW(TAG, "Nenhum áudio está sendo reproduzido.");
+        return;
+    }
+
+    playing = false;
     ESP_LOGI(TAG, "Parando áudio!");
 }
 
@@ -26,8 +60,9 @@ void audio_stop(void) {
 void audio_set_volume(uint8_t volume) {
     if(volume > 100) {
         ESP_LOGW(TAG, "Volume %d inválido! Ajustando para 100.", volume);
-        current_volume = 100;
+        volume = 100;
     }
 
     ESP_LOGI(TAG, "Alterando o volume atual de %d para %d%%", current_volume, volume);
+    current_volume = volume;
 }
--- Audio/main/audio_manager.h
+++ Audio/main/audio_manager.h
@@ -1,11 +1,16 @@
 #ifndef AUDIO_MANAGER_H
 #define AUDIO_MANAGER_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 void audio_init(void);
 
-void audio_play(void);
+bool audio_is_playing(void);
+
+void audio_play_default(void);
+
+void audio_play(const char *file);
 
 void audio_stop(void);
 
