fe14d1244e02fce61ac9933ab8f06e34b0ca99a2
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2026-07-07 09:34:07 -0300

    fix[2.2]: refatora server.c

--- Audio/main/server.c
+++ Audio/main/server.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifndef TEST_ENV
+#include <microhttpd.h>
 #include <json-c/json.h>
+#include <glib.h>
 
 #define POSTBUFFERSIZE 512
 
@@ -11,8 +15,8 @@ struct connection_info {
     size_t size;
 };
 
-// Processa o JSON após receber todo o corpo do POST:
-static void handle_audio_command(AudioManager *am, const char *json_str) {
+
+static void handle_audio_command(const char *json_str) {
     struct json_object *parsed_json = json_tokener_parse(json_str);
     if (!parsed_json) {
         g_printerr("Erro ao parsear JSON recebido!\n");
@@ -25,7 +29,7 @@ static void handle_audio_command(AudioManager *am, const char *json_str) {
         
         switch (id) {
             case 40:
-                audio_manager_stop(am);
+                audio_stop();
                 break;
 
             case 41: {
@@ -35,7 +39,7 @@ static void handle_audio_command(AudioManager *am, const char *json_str) {
                     file_name = json_object_get_string(file_obj);
                 }
 
-                audio_manager_start_playlist(am, file_name);
+                audio_play(file_name);
                 break;
             }
 
@@ -43,9 +47,9 @@ static void handle_audio_command(AudioManager *am, const char *json_str) {
                 struct json_object *vol_obj;
                 if (json_object_object_get_ex(parsed_json, "volume", &vol_obj)) {
                     int volume = json_object_get_int(vol_obj);
-                    audio_manager_set_volume(am, volume);
+  
+                    audio_set_volume((int8_t)volume);
                 }
-
                 break;
             }
 
@@ -63,7 +67,6 @@ static enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *co
                                            const char *url, const char *method,
                                            const char *version, const char *upload_data,
                                            size_t *upload_data_size, void **con_cls) {
-    HttpServer *server = (HttpServer *)cls;
     if (strcmp(url, "/audio") != 0 || strcmp(method, "POST") != 0) {
         struct MHD_Response *response = MHD_create_response_from_buffer(17, "Endpoint invalido", MHD_RESPMEM_PERSISTENT);
         MHD_add_response_header(response, "Content-Type", "text/plain");
@@ -72,7 +75,6 @@ static enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *co
         return ret;
     }
 
-    // Primeira chamada da requisição: aloca o buffer para receber o JSON:
     if (*con_cls == NULL) {
         struct connection_info *con_info = malloc(sizeof(struct connection_info));
         con_info->data = NULL;
@@ -83,7 +85,6 @@ static enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *co
 
     struct connection_info *con_info = (struct connection_info *)*con_cls;
 
-    // Se ainda houver dados chegando no POST, acumula no buffer:
     if (*upload_data_size != 0) {
         con_info->data = realloc(con_info->data, con_info->size + *upload_data_size + 1);
         memcpy(con_info->data + con_info->size, upload_data, *upload_data_size);
@@ -93,9 +94,8 @@ static enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *co
         return MHD_YES;
     }
 
-    // Todo o JSON foi recebido:
     if (con_info->data != NULL) {
-        handle_audio_command(server->am, con_info->data);
+        handle_audio_command(con_info->data);
         free(con_info->data);
     }
 
@@ -103,25 +103,31 @@ static enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *co
 
     const char *reply_str = "OK\n";
     struct MHD_Response *response = MHD_create_response_from_buffer(strlen(reply_str), (void *)reply_str, MHD_RESPMEM_PERSISTENT);
-    
     MHD_add_response_header(response, "Content-Type", "text/plain");
-    
     enum MHD_Result ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
     MHD_destroy_response(response);
     return ret;
 }
-
+#endif
 
 bool server_start(HttpServer *server, int port, AudioManager *am) {
     server->am = am;
+#ifndef TEST_ENV
     server->http_daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, port, NULL, NULL,
                                       &answer_to_connection, server, MHD_OPTION_END);
     return (server->http_daemon != NULL);
+#else
+    printf("[INFO] Mock Server: Servidor HTTP simulado com sucesso na porta %d.\n", port);
+    return true;
+#endif
 }
 
-
 void server_stop(HttpServer *server) {
+#ifndef TEST_ENV
     if (server->http_daemon) {
         MHD_stop_daemon(server->http_daemon);
     }
+#else
+    printf("[INFO] Mock Server: Servidor HTTP simulado finalizado.\n");
+#endif
 }
