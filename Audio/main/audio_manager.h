432cb34b968e78e846ab1c6f9dc5b941c24e6768
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2026-07-07 09:24:33 -0300

    fix[2.2]: refatora audio_manager.h

--- Audio/main/audio_manager.h
+++ Audio/main/audio_manager.h
@@ -1,22 +1,32 @@
 #ifndef AUDIO_MANAGER_H
 #define AUDIO_MANAGER_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifndef TEST_ENV
 #include <glib.h>
 #include <gst/gst.h>
-#include <stdbool.h>
+#endif
 
 typedef struct {
+#ifndef TEST_ENV
     GstElement *pipeline;
     GstElement *source;
     GstElement *volume_element;
     GMainLoop *loop;
-    
+#else
+    void *pipeline;
+    void *source;
+    void *volume_element;
+    void *loop;
+#endif
     bool is_playing;
-    int current_track; // 0 para Água Viva, 1 para Pôr do Sol
-    double current_volume; // 0.0 a 1.0
+    int current_track; 
+    double current_volume; 
 } AudioManager;
 
-void audio_manager_init(AudioManager *am, GMainLoop *loop);
+void audio_manager_init(AudioManager *am, void *loop);
 
 void audio_manager_start_playlist(AudioManager *am, const char *initial_file);
 
@@ -25,5 +35,17 @@ void audio_manager_stop(AudioManager *am);
 void audio_manager_set_volume(AudioManager *am, int volume_percent);
 
 void audio_manager_cleanup(AudioManager *am);
+
+void audio_init(void);
+
+void audio_play(const char *filename);
+
+void audio_play_default(void);
+
+void audio_stop(void);
+
+bool audio_is_playing(void);
+
+void audio_set_volume(int8_t volume);
 
 #endif
