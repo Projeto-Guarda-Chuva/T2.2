35929f48c282c11365001ed9426da6f042e6122f
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2026-07-07 09:32:09 -0300

    fix[2.2]: refatora server.h

--- Audio/main/server.h
+++ Audio/main/server.h
@@ -1,13 +1,22 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <microhttpd.h>
+#include <stdbool.h>
 #include "audio_manager.h"
 
+#ifndef TEST_ENV
+#include <microhttpd.h>
 typedef struct {
     struct MHD_Daemon *http_daemon;
     AudioManager *am;
 } HttpServer;
+#else
+
+typedef struct {
+    void *http_daemon;
+    void *am;
+} HttpServer;
+#endif
 
 bool server_start(HttpServer *server, int port, AudioManager *am);
 
