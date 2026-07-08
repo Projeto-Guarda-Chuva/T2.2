e04625fb6516777773c0b7666b1dcc6b4f459962
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2026-07-08 16:17:21 -0300

    fix[2.2]: refatora warnings de compilação e estabiliza mock HTTP

--- Iluminacao/Tela/main/source/protocol.c
+++ Iluminacao/Tela/main/source/protocol.c
@@ -24,9 +24,29 @@ void process_command_json(const char *json_str) {
                 int g = cJSON_GetObjectItem(root, "g") ? cJSON_GetObjectItem(root, "g")->valueint : 0;
                 int b = cJSON_GetObjectItem(root, "b") ? cJSON_GetObjectItem(root, "b")->valueint : 0;
 
-                if (r > 255) r = 255; if (r < 0) r = 0;
-                if (g > 255) g = 255; if (g < 0) g = 0;
-                if (b > 255) b = 255; if (b < 0) b = 0;
+                if(r > 255){
+                    r = 255;
+                } 
+                
+                if(r < 0) {
+                    r = 0;
+                }
+
+                if(g > 255) {
+                    g = 255;
+                } 
+                
+                if(g < 0) {
+                    g = 0;
+                }
+
+                if(b > 255) {
+                    b = 255;
+                } 
+                
+                if(b < 0) {
+                    b = 0;   
+                }
 
                 display_set_bg_color(r, g, b);
                 break;
